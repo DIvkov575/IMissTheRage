afd13400d62accef209d1109832d0e49767b6219
Divkov575 <dmitriy@ivkov.net>
2023-08-29 18:18:04 -0400

    removed serial writing

--- main.ino
+++ main.ino
@@ -15,16 +15,8 @@ void loop() {
   IMU.readGyroscope(Ax, Ay, Az); // functionality reversed?
 
   if ((abs(Ax) > threshold) || (abs(Ay) > threshold) || (abs(Az) > threshold)) {
-    Serial.println("=============================================================delete=============================================================");
     Keyboard.key_code_raw(0x1d, 0x08);
   }
-
-  Serial.print(Ax);
-  Serial.print('\t');
-  Serial.print(Ay);
-  Serial.print('\t');
-  Serial.print(Az);
-  Serial.print('\n');
 
   delay(250);
 }
