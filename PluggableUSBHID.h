7e4a95929a66d75588d72f9f70185c0f5e7097de
Dima I <79413560+DIvkov575@users.noreply.github.com>
2024-09-29 00:24:08 +0200

    commenting an uncommented line in pluggalbleUSBHID.h

--- PluggableUSBHID.h
+++ PluggableUSBHID.h
@@ -1,4 +1,4 @@
-tributed on an "AS IS" BASIS,
+/* tributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
